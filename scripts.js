f66a82292fa312305f09929936bd50ddf20bb320
Philip Huang <philiphuang2@gmail.com>
2022-10-02 00:38:20 -0700

    when I press the button, the button should dissapear and the timer should remain

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
 var button = document.querySelector("#start");
+var clock = document.querySelector('#clock');
 
 button.addEventListener("click",function(){
+    
     console.log("testing button");
-})
+    startTime();
+    button.parentElement.remove();
+});
+
+function startTime() {
+    var time  = 61;
+
+    var timeInterval = setInterval(function() {
+        time = time - 1;
+        if(time > 0)
+            clock.textContent = time;
+        else
+        {
+            clock.textContent = "";
+            clearInterval(timeInterval);
+        }
+    },1000);
+
+}
 
