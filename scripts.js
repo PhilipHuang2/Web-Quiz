2be1af8aca5807c4674662cdc0c73f11a6b156ff
Philip Huang <philiphuang2@gmail.com>
2022-10-02 00:58:06 -0700

    added functionality, when I press start, the button will dissappear and the timer will start.

--- scripts.js
+++ scripts.js
@@ -1,20 +1,29 @@
 var button = document.querySelector("#start");
-var clock = document.querySelector('#clock');
+var timeTitle = document.createElement("h2");
+var timer = document.createElement("h2");
+var timeLeft = 60;
+timeTitle.textContent = "Time Remaining:"
+timer.textContent = timeLeft;
 
-button.addEventListener("click",function(){
-    
+// When I click the button, delete the start button
+// and add 2 h2 tags, and start counting down
+button.addEventListener("click",function(){    
     console.log("testing button");
+    
+    document.querySelector("main").prepend(timer);
+    document.querySelector("main").prepend(timeTitle);
     startTime();
     button.parentElement.remove();
 });
 
+// creates a timer that counts down until 0
 function startTime() {
-    var time  = 61;
-
     var timeInterval = setInterval(function() {
-        time = time - 1;
-        if(time > 0)
-            clock.textContent = time;
+        timeLeft = timeLeft -1;
+        console.log(timer);
+        console.log(timeLeft);
+        if(timeLeft > 0)
+            timer.textContent = timeLeft.toString();
         else
         {
             clock.textContent = "";
